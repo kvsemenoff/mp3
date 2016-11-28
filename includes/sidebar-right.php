3e46dda1356120289d7e0b547404a197da0fd00d
engine77 <zaur.ka.00@mail.ru>
2016-11-28 12:51:49 +0300

    28

--- includes/sidebar-right.php
+++ includes/sidebar-right.php
@@ -1,1 +1,85 @@
-sidebar-right
+<div class="df-topcaption">
+	<span class="dfimg"></span>
+	<span class="df-cap">Иностранный топ</span>
+</div>
+
+<div class="df-music">
+	<span class="dfimg"><a href="#"></a></span>
+	<span class="df-num">1</span>
+	<div class="df-group">
+		<span>Юлиана Караулова</span>
+		<span>Разбитая Любовь (Project)</span>
+	</div>
+</div>
+<div class="df-music">
+	<span class="dfimg"><a href="#"></a></span>
+	<span class="df-num">2</span>
+	<div class="df-group">
+		<span>Julian Calor & Manse</span>
+		<span>Atlas (Original Mix)</span>
+	</div>
+</div>
+<div class="df-music">
+	<span class="dfimg"><a href="#"></a></span>
+	<span class="df-num">3</span>
+	<div class="df-group">
+		<span>Eric Saade Feat. Gustaf</span>
+		<span>Wide Awake (Red Mix)</span>
+	</div>
+</div>
+<div class="df-music">
+	<span class="dfimg"><a href="#"></a></span>
+	<span class="df-num">4</span>
+	<div class="df-group">
+		<span>Ostrovskaya</span>
+		<span>Просто Рядом Быть</span>
+	</div>
+</div>
+<div class="df-music">
+	<span class="dfimg"><a href="#"></a></span>
+	<span class="df-num">5</span>
+	<div class="df-group">
+		<span>Ollin Kan Feat. Liam Chan</span>
+		<span>Quiet Nights (Florian Paoid)</span>
+	</div>
+</div>
+<div class="df-music">
+	<span class="dfimg"><a href="#"></a></span>
+	<span class="df-num">6</span>
+	<div class="df-group">
+		<span>Nicco & Solid, Sound</span>
+		<span>Gunned Down (Exteded Mix)</span>
+	</div>
+</div>
+<div class="df-music">
+	<span class="dfimg"><a href="#"></a></span>
+	<span class="df-num">7</span>
+	<div class="df-group">
+		<span>Людмила Соколова</span>
+		<span>Я Буду Для Тебя</span>
+	</div>
+</div>
+<div class="df-music">
+	<span class="dfimg"><a href="#"></a></span>
+	<span class="df-num">8</span>
+	<div class="df-group">
+		<span>Олег Майами</span>
+		<span>Останься со мной навсегда</span>
+	</div>
+</div>
+<div class="df-music">
+	<span class="dfimg"><a href="#"></a></span>
+	<span class="df-num">9</span>
+	<div class="df-group">
+		<span>Eric Saade Feat. Gustaf</span>
+		<span>Wide Awake (Red Mix)</span>
+	</div>
+</div>
+<div class="df-music">
+	<span class="dfimg"><a href="#"></a></span>
+	<span class="df-num">10</span>
+	<div class="df-group">
+		<span>Ollin Kan Feat. Liam Chan</span>
+		<span>Quiet Nights (Florian Pazid)</span>
+	</div>
+</div>
