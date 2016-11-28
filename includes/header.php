1c57f417104fcc37795d8ea743e537c0d880a769
Berzeg07 <berezg86@gmail.com>
2016-11-28 12:11:32 +0300

    28.11

--- includes/header.php
+++ includes/header.php
@@ -1,9 +1,20 @@
 <section class="dd-header">
 	<div class="container">
 		<div class="row">
-			<div class="col-md-12">
+			<div class="col-md-2">
+				<a href="#"><img src="img/logo.png"></a>
+			</div>
+			<div class="col-md-8">
+				<div class="dd-poisk">
+					<label>
+						<input class="ab-in-put" type="text" placeholder="Поиск музыки...">
+					</label>
+				</div>
+			</div>
+			<div class="col-md-2">
 				header
 			</div>
+			<div class="clerafix"></div>	
 		</div>
 	</div>
 </section>
