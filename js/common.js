798ec520b056daadd0403689294d1b482f8da524
unknown <anzarsh@mail.ru>
2016-11-28 16:45:13 +0300

    common.js 1

--- js/common.js
+++ js/common.js
@@ -34,6 +34,24 @@ $(document).ready(function(){
         $(this).toggleClass('az-select-focus');
     });
 
+
+
+    // for popup
+    $('a[name=modal]').click(function(e) {
+        e.preventDefault();
+        var id = $(this).attr('href');
+        var maskHeight = $(document).height();
+        var maskWidth = $(window).width();
+        $('#mask').css({'width':maskWidth,'height':maskHeight});
+        $('#mask').fadeTo("slow",0.8); 
+        var winH = $(window).height();
+        var winW = $(window).width();
+        posTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
+        $(id).css('top',  posTop+150);
+        $(id).css('left', winW/2-$(id).width()/2);        
+        $(id).fadeIn(500); 
+    });
+
  });
 
 
