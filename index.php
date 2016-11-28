0d48d9a4e5899992bbd389d388ff6c5c1c10fb62
unknown <anzarsh@mail.ru>
2016-11-28 16:54:53 +0300

    popup 1

--- index.php
+++ index.php
@@ -25,15 +25,15 @@
 	<script type="text/javascript" src="js/jquery.jscrollpane.min.js"></script>
 
 	<script>
-    	new WOW().init();
-    </script>
+		new WOW().init();
+	</script>
 
 	<script src="js/responsiveTabs.js"></script>
 	<script src="js/jquery.maskedinput.min.js"></script>
 	<script src="js/common.js"></script>
 	
 	<?php require_once('includes/header.php'); ?>
-		
+	
 	<div class="container">
 		<div class="row">
 			<div class="col-md-3">
@@ -54,7 +54,20 @@
 	</div>
 
 	<?php require_once('includes/footer.php'); ?>
-
+	
+	<div id="mask"></div>
+	<div id="zvonok" class="window"> 
+		<form action="" class="common-form form1">
+			<div class="an-exit">
+				<span class="an-exit__krest"></span>
+			</div>
+			<span class="form-title"><?php the_field('заказать_звонок_текст_1'); ?></span>
+			<span class="form-subtitle"><?php the_field('заказать_звонок_текст_2'); ?></span>
+			<input type="text" name="tel" placeholder="+7 900 000 00 00" class="phone">
+			<input type="hidden" name="formname" value="Форма закзать констультацию">
+			<input type="submit" id="form1_submit" value="<?php the_field('заказать_звонок_кнопка'); ?>">
+		</form>
+	</div>
 	
 </body>
 </html>
